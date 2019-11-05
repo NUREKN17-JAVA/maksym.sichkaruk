1550aaea5e55650db3a5bc62fd3c2b9970bb811f
MaksymSihckaruk <maksym.sichkaruk@nure.ua>
2019-11-05 22:20:44 +0200

    step 2

---
+++ usermanage/src/Main/java/ua/nure/ITKN179MaksimSichkaruk/userManagment/db/HsqldbUserDao.java
@@ -0,0 +1,43 @@
+package ua.nure.ITKN179MaksimSichkaruk.userManagment.db;
+
+import java.util.Collection;
+
+import ua.nure.ITKN179MaksimSichkaruk.userManagment.User;
+
+public class HsqldbUserDao implements Dao<User> {
+
+	public HsqldbUserDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public User create(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User find(long id) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<User> findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
