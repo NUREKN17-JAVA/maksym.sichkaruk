fe750b9a1f150ab9aa3019164b1df3c1caae84bd
MaksymSihckaruk <maksym.sichkaruk@nure.ua>
2019-11-05 22:30:41 +0200

    implementation of a database connection factory

---
+++ usermanage/src/Main/java/ua/nure/ITKN179MaksimSichkaruk/userManagment/db/ConnectionFactory.java
@@ -0,0 +1,7 @@
+package ua.nure.ITKN179MaksimSichkaruk.userManagment.db;
+
+import java.sql.Connection;
+
+public interface ConnectionFactory {
+	Connection createConnection() throws DatabaseException;
+}
