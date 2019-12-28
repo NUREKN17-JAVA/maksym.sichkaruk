58ad1342373c92d0320d7f626d7b61789f98e5d7
Maksus <maksym.sichkaruk@nure.ua>
2019-12-28 12:04:02 +0200

    5 lab

---
+++ src/Main/java/ua/nure/itkn179/sichkaruk/userManagment/agent/exceptions/SearchException.java
@@ -0,0 +1,7 @@
+package Main.java.ua.nure.itkn179.sichkaruk.userManagment.agent.exceptions;
+
+public class SearchException extends Exception {
+    public SearchException(String message) {
+        super(message);
+    }
+}
